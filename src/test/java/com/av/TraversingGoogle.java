e6cf8c9118104e2a18d85c3ff0b0102b0422f166
Kumar Abhinav <59536541+kumarabhinv@users.noreply.github.com>
2026-03-09 18:25:03 +0530

    Pushing playwright framework (#1)
    * Adding POM with basic Playwright, Maven Surefire and Junit properties
    
    * project structure
    
    * Basic framwork
    
    * Pushing Playwright Framework with basic functionality
    
    * added sysout
    
    * added readme from remote
    
    ---------
    
    Co-authored-by: Kumar Abhinav <Kumar.Abhinav2@ibm.com>

---
+++ src/test/java/com/av/TraversingGoogle.java
@@ -0,0 +1,13 @@
+package com.av;
+
+import org.testng.annotations.Test;
+
+public class TraversingGoogle extends Base {
+
+    @Test
+    private void traversingGoogle() {
+        utils().findByRoleAsLink("AI Mode").click();
+        System.out.println("Test Complete");
+    }
+
+}
